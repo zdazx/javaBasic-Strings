27fffa0c64a58996705c815c45d720fb045e6fe0
zdazx <zhangduo0227@163.com>
2020-07-16 17:43:23 +0800

    string replace() will return a new variable

--- src/test/java/com/cultivation/javaBasic/StringTest.java
+++ src/test/java/com/cultivation/javaBasic/StringTest.java
@@ -17,8 +17,9 @@ class StringTest {
         // TODO: Please modify the following line to pass the test.
         //
         // It is really easy to pass the test. But you have to tell why.
+        // String replace() will return a new variable.
         // <--start
-        final Optional<Boolean> areSame = Optional.empty();
+        final Optional<Boolean> areSame = Optional.of(false);
         // --end-->
 
         assertEquals("The new string", modifiedString);
