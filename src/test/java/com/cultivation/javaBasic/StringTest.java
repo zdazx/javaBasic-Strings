ce8ed27f07c1d02775e1dc3364dd5a87b4f0c95d
zdazx <zhangduo0227@163.com>
2020-07-16 18:00:54 +0800

    calculate checksum of a string with toCharArray()

--- src/test/java/com/cultivation/javaBasic/StringTest.java
+++ src/test/java/com/cultivation/javaBasic/StringTest.java
@@ -154,6 +154,9 @@ class StringTest {
         int sum = 0;
         // TODO: Write some code to calculate the checksum of the string. The checksum is the sum of each string char.
         // <--Start
+        for (char c : text.toCharArray()) {
+            sum += c;
+        }
         // --End-->
 
         assertEquals(3655, sum);
